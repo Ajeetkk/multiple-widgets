ac679a941bf45addf099e350913a880e8ba1446c
EXTERNAL Kurrey Ajeet Kumar (Datamatics <ukj2kor@bosch.com>
2019-09-23 12:49:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hi Team
+    </div>
+  );
+}
+function SecondWidgets(){
+  return (
+    <div>
+      This is our multiple widgets implemantion 
     </div>
   );
 }
 
-export default App;
+export {App, SecondWidgets};
